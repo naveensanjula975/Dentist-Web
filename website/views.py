bde38a6e4972a4c2d388da90cff3aa1878c8e5fd
naveen sanjula <naveensanjula975@gmail.com>
2022-07-16 11:28:58 +0530

    Appointment

--- website/views.py
+++ website/views.py
@@ -33,3 +33,37 @@ def pricing(request):
 
 def service(request):
     return render(request, 'service.html', {})
+
+def appointment(request):
+    if request.method == "POST":
+        your_name = request.POST['your-name']
+        your_phone = request.POST['your-phone']
+        your_email = request.POST['your-email']
+        your_address = request.POST['your-address']
+        your_schedule = request.POST['your-schedule']
+        your_date = request.POST['your-date']
+        your_message = request.POST['your-message']
+
+        # send an email
+
+        appointment = "Name: " + your_name + " Phone: " + your_phone + "Email: " + your_email + " Address: "  + your_address + " Schedule: " + your_schedule + " Date: " + your_date + " Message: " + your_message
+        send_mail(
+            'Appointment Request', # subject
+            appointment, # message
+            your_email, # from email
+            ['naveen@email.com'], # To Email
+        )
+        
+        return render(request, 'appointment.html', {
+            'your_name': your_name,
+            'your_phone': your_phone,
+            'your_email': your_email,
+            'your_address': your_address,
+            'your_schedule': your_schedule,
+            'your_date': your_date,
+            'your_message': your_message,
+            })
+
+
+    else:
+        return render(request, 'home.html', {})
